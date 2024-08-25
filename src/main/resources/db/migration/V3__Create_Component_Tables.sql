9e8bb5be52d2213f7377b9ccc50ebe82bbe11b98
fernandacorreasz <fernanda_1813@outlook.com>
2024-08-25 15:29:42 -0300

    fix/tag-011: Adjust component migration script with audit and version control columns

--- src/main/resources/db/migration/V3__Create_Component_Tables.sql
+++ src/main/resources/db/migration/V3__Create_Component_Tables.sql
@@ -2,7 +2,13 @@
 CREATE TABLE component_category (
     id UUID PRIMARY KEY,
     category_id VARCHAR(255) NOT NULL,
-    category_name VARCHAR(255) NOT NULL
+    category_name VARCHAR(255) NOT NULL,
+    -- Audit and version control columns
+    row_version SMALLINT NOT NULL DEFAULT 0,
+    row_created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    row_updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    row_created_by VARCHAR(64) NOT NULL DEFAULT 'unknown',
+    row_updated_by VARCHAR(64) NOT NULL DEFAULT 'unknown'
 );
 
 -- Create table for ComponentSubCategory
@@ -13,6 +19,12 @@ CREATE TABLE component_sub_category (
     sub_category_name VARCHAR(255) NOT NULL,
     sub_category_image BYTEA,
     category_id UUID,
+    -- Audit and version control columns
+    row_version SMALLINT NOT NULL DEFAULT 0,
+    row_created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    row_updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    row_created_by VARCHAR(64) NOT NULL DEFAULT 'unknown',
+    row_updated_by VARCHAR(64) NOT NULL DEFAULT 'unknown',
     CONSTRAINT fk_category
         FOREIGN KEY (category_id)
         REFERENCES component_category(id)
@@ -28,6 +40,12 @@ CREATE TABLE component (
     quantity INTEGER NOT NULL,
     sub_category_id UUID,
     category_id UUID,
+    -- Audit and version control columns
+    row_version SMALLINT NOT NULL DEFAULT 0,
+    row_created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    row_updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    row_created_by VARCHAR(64) NOT NULL DEFAULT 'unknown',
+    row_updated_by VARCHAR(64) NOT NULL DEFAULT 'unknown',
     CONSTRAINT fk_sub_category
         FOREIGN KEY (sub_category_id)
         REFERENCES component_sub_category(id),
@@ -41,6 +59,12 @@ CREATE TABLE component_test (
     id UUID PRIMARY KEY,
     test_description TEXT NOT NULL,
     component_id UUID,
+    -- Audit and version control columns
+    row_version SMALLINT NOT NULL DEFAULT 0,
+    row_created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    row_updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    row_created_by VARCHAR(64) NOT NULL DEFAULT 'unknown',
+    row_updated_by VARCHAR(64) NOT NULL DEFAULT 'unknown',
     CONSTRAINT fk_component
         FOREIGN KEY (component_id)
         REFERENCES component(id)
